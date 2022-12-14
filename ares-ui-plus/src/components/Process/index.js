3413599b3536f1721ca6408ae4e0aa70496b18ff
Young <1065569578@qq.com>
2022-12-14 16:52:44 +0800

    20221214

--- ares-ui-plus/src/components/Process/index.js
+++
@@ -1,6 +0,0 @@
-import workflowBpmnModeler from './index.vue'
-
-workflowBpmnModeler.install = (Vue) =>
-  createApp(App).component(workflowBpmnModeler.name, workflowBpmnModeler) // 给组件配置install方法
-
-export default workflowBpmnModeler
