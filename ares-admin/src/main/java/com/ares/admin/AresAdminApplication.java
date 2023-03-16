556cadd52d4620c74006b284394c8aca8106b386
Young <1065569578@qq.com>
2023-03-16 15:06:34 +0800

    20230316

--- ares-admin/src/main/java/com/ares/admin/AresAdminApplication.java
+++ ares-admin/src/main/java/com/ares/admin/AresAdminApplication.java
@@ -1,10 +1,15 @@
 package com.ares.admin;
 
+import cn.dev33.satoken.dao.SaTokenDaoRedisJackson;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceAutoConfiguration;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import springfox.boot.starter.autoconfigure.OpenApiAutoConfiguration;
 
 /**
  * @description:
@@ -14,7 +19,9 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 @EnableAdminServer
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, JdbcRepositoriesAutoConfiguration.class,
+        OpenApiAutoConfiguration.class, DynamicDataSourceAutoConfiguration.class,
+        SaTokenDaoRedisJackson.class,RedisAutoConfiguration.class})
 public class AresAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AresAdminApplication.class, args);
