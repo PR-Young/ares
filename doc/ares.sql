1129a3d52fe1a2a79f79f47fe7cab3c0e8ff09bd
Young <1065569578@qq.com>
2023-07-20 09:15:23 +0800

    20230720

--- doc/ares.sql
+++ doc/ares.sql
@@ -824,4 +824,4 @@ CREATE TABLE `sys_login_info` (
 SET FOREIGN_KEY_CHECKS = 1;
 
 
-INSERT INTO sys_menu` (`Id`, `Name`, `Description`, `Path`, `Url`, `IsBlank`, `PId`, `Icon`, `Order`, `Visible`, `Perms`, `Type`, `Creator`, `CreateTime`, `Modifier`, `ModifyTime`) VALUES ('841514856120193024', '报表', NULL, 'report', '/tool/report/index', NULL, '383838051287306240', 'dashboard', 9, 0, '#', 1, '1', '2023-07-19 05:26:27', NULL, NULL);
+INSERT INTO `sys_menu` (`Id`, `Name`, `Description`, `Path`, `Url`, `IsBlank`, `PId`, `Icon`, `Order`, `Visible`, `Perms`, `Type`, `Creator`, `CreateTime`, `Modifier`, `ModifyTime`) VALUES ('841514856120193024', '报表', NULL, 'report', '/tool/report/index', NULL, '383838051287306240', 'dashboard', 9, 0, '#', 1, '1', '2023-07-19 05:26:27', NULL, NULL);
