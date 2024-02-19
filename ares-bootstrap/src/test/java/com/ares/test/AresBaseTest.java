df37b0793a31b5c7e5fe3b8c9541202f0273bb95
Young <1065569578@qq.com>
2024-02-19 14:27:35 +0800

    20240219|增加启动模块

---
+++ ares-bootstrap/src/test/java/com/ares/test/AresBaseTest.java
@@ -0,0 +1,43 @@
+/*
+ *
+ *  * ****************************************************************************
+ *  * * Copyright (c) 2021 - 9999, ARES
+ *  * *
+ *  * * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * * you may not use this file except in compliance with the License.
+ *  * * You may obtain a copy of the License at
+ *  * *
+ *  * *        http://www.apache.org/licenses/LICENSE-2.0
+ *  * *
+ *  * * Unless required by applicable law or agreed to in writing, software
+ *  * * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * * See the License for the specific language governing permissions and
+ *  * * limitations under the License.
+ *  * ***************************************************************************
+ *
+ */
+
+package com.ares.test;
+
+import com.ares.AresSystemApplication;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @description:
+ * @author: Young
+ * @date: 2021/03/01
+ * @see: com.ares.test AresBaseTest.java
+ **/
+@SpringBootTest(classes = AresSystemApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AresBaseTest {
+    protected static final Logger log = LoggerFactory.getLogger(AresBaseTest.class);
+
+    @Test
+    public void contextLoads() {
+        log.info("ok!");
+    }
+}
--- ares-bootstrap/src/test/java/com/ares/test/AresMessageTest.java
+++ ares-bootstrap/src/test/java/com/ares/test/AresMessageTest.java
@@ -1,19 +1,21 @@
 /*
- * !******************************************************************************
- *  * Copyright (c) 2021 - 9999, ARES
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  *        http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- *  *****************************************************************************
+ *
+ *  * ****************************************************************************
+ *  * * Copyright (c) 2021 - 9999, ARES
+ *  * *
+ *  * * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * * you may not use this file except in compliance with the License.
+ *  * * You may obtain a copy of the License at
+ *  * *
+ *  * *        http://www.apache.org/licenses/LICENSE-2.0
+ *  * *
+ *  * * Unless required by applicable law or agreed to in writing, software
+ *  * * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * * See the License for the specific language governing permissions and
+ *  * * limitations under the License.
+ *  * ***************************************************************************
+ *
  */
 
 package com.ares.test;
@@ -51,7 +53,7 @@ public class AresMessageTest extends AresBaseTest {
     public static void main(String[] args) throws InterruptedException {
         AresMessageHandler<String> handler = new AresMessageHandler<String>();
         DisruptorQueue<String> disruptorQueue = DisruptorQueueFactory.getHandleEventsQueue(1, false, handler);
-        AresMessageProducer<String> producer = new AresMessageProducer("生产者1", disruptorQueue, "test", new AtomicInteger(1));
+        AresMessageProducer<String> producer = new AresMessageProducer("生产者1", disruptorQueue, "com/ares/test", new AtomicInteger(1));
         Thread thread = new Thread(producer);
         thread.start();
 
@@ -61,7 +63,7 @@ public class AresMessageTest extends AresBaseTest {
 
     @Test
     public void test(){
-        AresMessageUtil.sendMessage("test","this is a test!");
+        AresMessageUtil.sendMessage("com/ares/test","this is a test!");
     }
 
 
