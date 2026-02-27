ff6cf40a16124bd57ebdca0d0677d4cfcf73448a
Young <1065569578@qq.com>
2026-02-27 10:04:20 +0800

    20260227|优化代码,修复流程bug

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,12 @@ LABEL authors="young"
 
 EXPOSE 8080
 
-ARG JAR_FILE=ares.jar
+ARG APP_FILE=ares-bin.zip
 
-ADD /ares-bootstrap/target/$JAR_FILE /ares.jar
+ADD /ares-bootstrap/target/$JAR_FILE /ares/ares-bin.zip
 
-ENTRYPOINT ["java", "-jar", "/ares.jar"]
+RUN sed -i 's#http://archive.ubuntu.com/#http://mirrors.tuna.tsinghua.edu.cn/#' /etc/apt/sources.list ;  \
+    apt-get update && apt-get install -y unzip ; \
+    unzip /ares/ares-bin.zip ; \
+
+ENTRYPOINT ["java", "-Dloader.path=/ares/lib" , "-jar", "/ares.jar"]
---
+++ ares-bootstrap/src/main/java/com/ares/bootstrap/exception/SysGlobalExceptionHandler.java
@@ -0,0 +1,115 @@
+/*
+ *
+ *  * ****************************************************************************
+ *  * * Copyright (c) 2021 - 9999, ARES
+ *  * *
+ *  * * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * * you may not use this file except in compliance with the License.
+ *  * * You may obtain a copy of the License at
+ *  * *
+ *  * *        http://www.apache.org/licenses/LICENSE-2.0
+ *  * *
+ *  * * Unless required by applicable law or agreed to in writing, software
+ *  * * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * * See the License for the specific language governing permissions and
+ *  * * limitations under the License.
+ *  * ***************************************************************************
+ *
+ */
+
+package com.ares.bootstrap.exception;
+
+
+import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.stp.StpUtil;
+import com.ares.core.common.exception.UserException;
+import com.ares.core.model.base.AjaxResult;
+import com.ares.core.model.base.Constants;
+import com.ares.core.model.base.ResultCode;
+import com.ares.core.model.exception.ErrorCode;
+import com.ares.core.model.vo.SysLoginInfo;
+import com.ares.core.service.ISysLoginInfoService;
+import com.ares.core.utils.SpringUtils;
+import com.ares.redis.utils.RedisUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.dromara.warm.flow.core.exception.FlowException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * @description:
+ * @author: Young 2020/05/09
+ **/
+@RestControllerAdvice
+@Order(value = Ordered.HIGHEST_PRECEDENCE)
+public class SysGlobalExceptionHandler {
+    private static final Set<String> NOT_LOGIN_EXCEPTION_TYPES = new HashSet<>(Arrays.asList(
+            NotLoginException.INVALID_TOKEN_MESSAGE,
+            NotLoginException.BE_REPLACED_MESSAGE,
+            NotLoginException.DEFAULT_MESSAGE,
+            NotLoginException.KICK_OUT_MESSAGE,
+            NotLoginException.TOKEN_TIMEOUT_MESSAGE,
+            NotLoginException.NOT_TOKEN_MESSAGE));
+
+
+    @ExceptionHandler(value = BindException.class)
+    public Object handleNotValidException(HttpServletRequest request, HttpServletResponse response, BindException e) {
+        ObjectError error = e.getBindingResult().getAllErrors().get(0);
+        return AjaxResult.result(ResultCode.VALIDATE_FAILED.getCode(), ResultCode.VALIDATE_FAILED.getMsg(), error.getDefaultMessage());
+    }
+
+    @ExceptionHandler(value = UserException.class)
+    public Object handleUserException(HttpServletRequest request, HttpServletResponse response, UserException e) {
+        Integer code = e.getCode();
+        if (code.equals(ErrorCode.NOUSER.getCode())) {
+            return AjaxResult.error(ResultCode.NOUSER.getCode(), ResultCode.NOUSER.getMsg());
+        } else if (code.equals(ErrorCode.NOAUTH.getCode())) {
+            return AjaxResult.unAuth();
+        }
+        return AjaxResult.unLogin();
+    }
+
+    @ExceptionHandler(value = NumberFormatException.class)
+    public Object handleNumberFormatException(HttpServletRequest request, HttpServletResponse response, NumberFormatException e) {
+        return AjaxResult.error(ResultCode.FAILED.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(value = PersistenceException.class)
+    public Object handlePersistenceException(HttpServletRequest request, HttpServletResponse response, PersistenceException e) {
+        return AjaxResult.error(ResultCode.FAILED.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(value = NotLoginException.class)
+    public Object handleException(HttpServletRequest request, HttpServletResponse response, NotLoginException e) {
+        if (null != e.getType() && NOT_LOGIN_EXCEPTION_TYPES.contains(e.getType())) {
+            String token = StpUtil.getTokenValue();
+            Long id = Long.valueOf(String.valueOf(RedisUtil.get(token)));
+            SysLoginInfo sysLoginInfo = new SysLoginInfo();
+            sysLoginInfo.setId(id);
+            sysLoginInfo.setStatus(Constants.OFFLINE);
+            ISysLoginInfoService loginInfoService = SpringUtils.getBean(ISysLoginInfoService.class);
+            loginInfoService.update(sysLoginInfo);
+            RedisUtil.del(token);
+            return AjaxResult.error(ResultCode.NOLOGIN.getCode(), ResultCode.NOLOGIN.getMsg());
+        }
+        return AjaxResult.error(ResultCode.FAILED.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(value = FlowException.class)
+    public Object handleFlowException(HttpServletRequest request, HttpServletResponse response, FlowException e) {
+        return AjaxResult.error(ResultCode.FAILED.getCode(), e.getMessage());
+    }
+
+}
